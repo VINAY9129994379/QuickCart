e5db807df04935df39e07bd5e1f21b721d8add57
VINAY9129994379 <vinay2272001@gmail.com>
2025-03-06 20:31:20 +0530

    first commit

---
+++ configs/db.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false
+        };
+
+        cached.promise = mongoose.connect(`${process.env.MONGO_URI}/QuickCart`, opts)
+            .then((mongoose) => mongoose)
+            .catch((err) => {
+                console.error("MongoDB connection error:", err);
+                cached.promise = null; // Reset promise to allow retrying
+                throw err;
+            });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (err) {
+        throw new Error("Failed to connect to database");
+    }
+
+    return cached.conn;
+}
+
+export default connectDB;
